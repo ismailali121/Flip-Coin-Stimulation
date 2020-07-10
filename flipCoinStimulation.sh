0a664e97278bbfa4e091e6efd689c42e30af7656
ismailali121 <pathanismailalikhan@gmail.com>
2020-07-10 17:04:35 +0530

    new

--- flipCoinStimulation.sh
+++ flipCoinStimulation.sh
@@ -1,28 +1,37 @@
 #!/bin/bash -x
 headCount=0
 tailCount=0
-while (($headCount!=21&&$tailCount!=21))
+flag=0
+while (($flag!=1))
 do
-	result=$((RANDOM%2))
-	if [ $result = 0 ]
+	while (($headCount!=21&&$tailCount!=21))
+	do
+		result=$((RANDOM%2))
+		if [ $result = 0 ]
+		then
+			((headCount++))
+			echo HEADS
+		else
+			((tailCount++))
+			echo TAILS
+		fi
+	done
+	echo HEADS has won $headCount times
+	echo TAILS has won $tailCount times
+	if [[ $headCount -gt $tailCount ]]
 	then
-		((headCount++))
-		echo HEADS
+		difference=$(($headCount - $tailCount))
+		if [$difference]
+		echo HEAD has WON by $difference times
+		#break
+	elif [[ $headCount -lt $tailCount && $difference -ge 2 ]]
+	then
+        	difference=$(($tailCount - $headCount))
+		echo TAIL has WON by $difference times
+		#break
 	else
-		((tailCount++))
-		echo TAILS
-	fi
+		echo Its a TIE
+		flag=1
+
+		fi
 done
-echo HEADS has won $headCount times
-echo TAILS has won $tailCount times
-if [[ $headCount -gt $tailCount ]]
-then
-	difference=$(($headCount - $tailCount))
-	echo HEAD has WON by $difference times
-elif [[ $headCount -lt $tailCount ]]
-then
-        difference=$(($tailCount - $headCount))
-	echo TAIL has WON by $difference times
-else
-	echo Its a TIE
-fi
