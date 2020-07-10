df6462689074acc004c7b61406adf9e85d2be2b4
ismailali121 <pathanismailalikhan@gmail.com>
2020-07-11 00:36:57 +0530

    Added program to repeat process if its a tie till difference is 2

--- flipCoinStimulation.sh
+++ flipCoinStimulation.sh
@@ -1,28 +1,76 @@
 #!/bin/bash -x
+function repeatAgain () {
 headCount=0
 tailCount=0
-while (($headCount!=21&&$tailCount!=21))
+while true
 do
-	result=$((RANDOM%2))
-	if [ $result = 0 ]
-	then
-		((headCount++))
-		echo HEADS
-	else
-		((tailCount++))
-		echo TAILS
-	fi
+        for((i=0;i<$noOfTimes;i++))
+        do
+                result=$((RANDOM%2))
+                if [ $result = 0 ]
+                then
+                        ((headCount++))
+                        echo HEADS
+                else
+                        ((tailCount++))
+                        echo TAILS
+                fi
+        done
+        echo HEADS has won $headCount times
+        echo TAILS has won $tailCount times
+        if [[ $headCount -gt $tailCount ]]
+        then
+                diff=$(($headCount - $tailCount))
+                if [ $diff -ge 2 ]
+                then
+                        echo HEAD has WON by $diff times
+                        break
+                else
+                        continue
+                fi
+        elif [[ $headCount -lt $tailCount ]]
+        then
+                diff=$(($tailCount - $headCount))
+                if [ $diff -ge 2 ]
+                then
+                        echo TAIL has WON by $diff times
+                        break
+                else
+                        continue
+                fi
+        else
+                echo "Its a TIE ( Trying Again )"
+                continue
+        fi
+done
+}
+
+read -p "How many times you want to flip a coin" noOfTimes
+headCount=0
+tailCount=0
+for((i=0;i<$noOfTimes;i++))
+do
+        result=$((RANDOM%2))
+        if [ $result = 0 ]
+        then
+                ((headCount++))
+                echo HEADS
+        else
+                ((tailCount++))
+                echo TAILS
+        fi
 done
 echo HEADS has won $headCount times
 echo TAILS has won $tailCount times
 if [[ $headCount -gt $tailCount ]]
 then
-	difference=$(($headCount - $tailCount))
-	echo HEAD has WON by $difference times
+        diff=$(($headCount - $tailCount))
+        echo HEAD has WON by $diff times
 elif [[ $headCount -lt $tailCount ]]
 then
-        difference=$(($tailCount - $headCount))
-	echo TAIL has WON by $difference times
+        diff=$(($tailCount - $headCount))
+        echo TAIL has WON by $diff times
 else
-	echo Its a TIE
+        echo "Its a TIE ( Trying Again )"
+        repeatAgain
 fi
