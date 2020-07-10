b0e0485ab127cfa6ae7377821e8274a7e5ae67fb
ismailali121 <pathanismailalikhan@gmail.com>
2020-07-10 15:10:20 +0530

    Added program to filp coin multiple times

--- flipCoinStimulation.sh
+++ flipCoinStimulation.sh
@@ -1,8 +1,18 @@
 #!/bin/bash -x
-result=$((RANDOM%2))
-if [ $result = 0 ]
-then
-	echo HEADS
-else
-	echo TAILS
-fi
+read -p "Enter how many times you want to flip coin : " noOfTimes
+headCount=0
+tailCount=0
+for((i=0;i<$noOfTimes;i++))
+do
+	result=$((RANDOM%2))
+	if [ $result = 0 ]
+	then
+		((headCount++))
+		echo HEADS
+	else
+		((tailCount++))
+		echo TAILS
+	fi
+done
+echo HEADS has won $headCount times
+echo TAILS has won $tailCount times
